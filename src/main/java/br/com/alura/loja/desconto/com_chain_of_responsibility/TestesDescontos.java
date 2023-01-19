ac2d2ce8c30b0c0fc21a6e9bd91e8cc403dcec73
Mel <melmelissa1936@gmail.com>
2023-01-18 22:47:19 -0300

    feat: using chain of responsibility pattern

---
+++ src/main/java/br/com/alura/loja/desconto/com_chain_of_responsibility/TestesDescontos.java
@@ -0,0 +1,16 @@
+package br.com.alura.loja.desconto.com_chain_of_responsibility;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class TestesDescontos {
+    public static void main(String[] args) {
+        Orcamento orcamento1 = new Orcamento(new BigDecimal("200"), 6);
+        Orcamento orcamento2 = new Orcamento(new BigDecimal("1000"), 1);
+        CalculadoraDeDescontos calculadoraDeDescontos = new CalculadoraDeDescontos();
+
+        System.out.println(calculadoraDeDescontos.calcular(orcamento1));
+        System.out.println(calculadoraDeDescontos.calcular(orcamento2));
+    }
+}
--- src/main/java/br/com/alura/loja/orcamento/Orcamento.java
+++ src/main/java/br/com/alura/loja/orcamento/Orcamento.java
@@ -3,13 +3,20 @@ package br.com.alura.loja.orcamento;
 import java.math.BigDecimal;
 
 public class Orcamento {
-    private BigDecimal valor;
 
-    public Orcamento(BigDecimal valor) {
+    private BigDecimal valor;
+    private int qtdItens;
+
+    public Orcamento(BigDecimal valor, int qtdItens) {
         this.valor = valor;
+        this.qtdItens = qtdItens;
     }
 
     public BigDecimal getValor() {
         return valor;
+    }
+
+    public int getQtdItens() {
+        return qtdItens;
     }
 }
